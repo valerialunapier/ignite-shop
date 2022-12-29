e7ec2ee0ea5549a661613656847a48da26c9f4f5
Valeria Luna <105656625+valerialunapier@users.noreply.github.com>
2022-12-29 16:40:28 -0300

    feat: add global styles

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,17 @@
 import type { AppProps } from 'next/app'
+import Image from 'next/image';
+import { globalStyles } from '../styles/global';
+import { Container, Header } from '../styles/pages/app';
+
+globalStyles();
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Container>
+      <Header>
+        <Image src="/images/logo.svg" width={130} height={50} alt="Logo ignite-shop" />
+      </Header>
+      <Component {...pageProps} />
+    </Container>
+  )
 }
